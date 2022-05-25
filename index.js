9856111a67b912da6ea4d195134e9d77cabbaf3b
joymohanta <joymohanta06@gmail.com>
2022-05-25 23:53:47 +0600

    admin api added

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ async function run() {
       res.send(tools);
     });
 
+    app.get("/user", async (req, res) => {
+      const allUsers = await userCollection.find().toArray();
+      res.send(allUsers);
+    });
+
     // Put method for get all users
     app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
@@ -50,6 +55,17 @@ async function run() {
         { expiresIn: "1h" }
       );
       res.send({ result, token });
+    });
+
+    // Put method for Admin users
+    app.put("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     // Get and findOne tool
