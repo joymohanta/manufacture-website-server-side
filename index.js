b464f0ba955f91eb312185d76d067704c6f51500
joymohanta <joymohanta06@gmail.com>
2022-06-02 03:07:34 +0600

    msg

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middle ware
 app.use(cors());
-app.use(cors({ origin: "https://new-drill-world.web.app/" }));
+app.use(cors({ origin: "new-drill-world.web.app/" }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.urvwn.mongodb.net/?retryWrites=true&w=majority`;
